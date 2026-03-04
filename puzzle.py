40a2f037cec5c3ac565a3295cb37e85256ce33e3
asneha0901 <agarwalsneha@uchicago.edu>
2026-03-04 00:40:04 -0600

    final set of spelling mistakes

--- puzzle.py
+++ puzzle.py
@@ -1,8 +1,8 @@
+
+
+puzzle_easy_key = 8983  
+puzzle_easy_misspell = "gadqens" 
+puzzle_key = "747585522"        
+puzzle_misspell = "cirupmstances"    
 cnet_id = "agarwalsneha"    
 nonce = "184650223" #for more details on how I did it also look at the work.ipynb file
-
-puzzle_easy_key="8983"
-puzzle_easy_missspell = "gadqens"
-
-puzzle_key = "747585522"
-puzzle_misspell = "cirupmstances"
