b264326f8d4b19af4f63e7b8241ce1aa5fdd9e92
asneha0901 <agarwalsneha@uchicago.edu>
2026-03-03 23:46:53 -0600

    proof of work, puzzle easy done

---
+++ puzzle.py
@@ -0,0 +1,6 @@
+cnet_id = "agarwalsneha"    
+nonce = "184650223" #for more details on how I did it also look at the work.ipynb file
+
+puzzle_easy_key="0008983"
+puzzle_easy_missspelling = "gadqens"
+
