1c8b2e85f7645e809276daa308f6a4a6266b17eb
asneha0901 <agarwalsneha@uchicago.edu>
2026-03-04 00:28:32 -0600

    CRACKED PUZZLE - NEED TO TYPE OUT PROCESS

--- puzzle.py
+++ puzzle.py
@@ -4,3 +4,5 @@ nonce = "184650223" #for more details on how I did it also look at the work.ipyn
 puzzle_easy_key="0008983"
 puzzle_easy_missspelling = "gadqens"
 
+puzzle_key = "747585522"
+puzzle_mispell = "cirupmstances"
